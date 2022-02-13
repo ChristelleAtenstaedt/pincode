afb892652aa00baf0c28db576bb03925deeefee2
Orlane <orlanedoumbe@gmail.com>
2022-02-13 21:22:00 +0100

    Orlane added her version

--- qu2.py
+++ qu2.py
@@ -13,3 +13,20 @@ else:
     print('Access banned - 3 attempts, please contact your bank to reset your PIN.')
     
     this has been changed
+
+# Orlane's version
+
+import getpass
+
+
+PIN = 6924
+max_attempts = 3
+
+while max_attempts > 0:
+    supplied_pin = getpass.getpass("Enter your PIN: ")
+    if int(supplied_pin) != PIN:
+        max_attempts -= 1
+        print("Incorrect PIN. You have", max_attempts, "attempts left.")
+    else:
+        print("Success!")
+        break
